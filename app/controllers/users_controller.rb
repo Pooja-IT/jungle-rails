038663618a50ee9eff63d423cddb2e26b83f16fa
Pooja Patel <poojapatel.web@gmail.com>
2021-09-21 13:41:26 -0400

    add user authentication feature signup and login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    p params
+    @user = User.new(user_params)
+    
+    if @user.save
+      session[:user_id] = @user.id 
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email.downcase, :password, :password_confirmation)
+  end
+end
